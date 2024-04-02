9b2e44929d15b075f37b4880ecb0c9786f5222cc
Arif Gökhan Araç <131158816+arifGokhan@users.noreply.github.com>
2024-04-02 22:12:39 +0300

    Update main.py

--- version/main.py
+++ version/main.py
@@ -1,1 +1,12 @@
+import micropython_ota
 
+# connect to network
+do_connect()
+
+ota_host = 'https://iot.isabetonline.com'
+project_name = 'micropython'
+filenames = ['boot.py', 'main.py']
+user = 'isabeton'
+passwd = '993399_aRiF' # it's best to place this credential is a secrets.py file
+
+micropython_ota.ota_update(ota_host, project_name, filenames, user=user, passwd=passwd, use_version_prefix=True, hard_reset_device=True, soft_reset_device=False, timeout=5)
